ae0105cfee68988be688f4ca170be7bb6e7ba1c4
jayavardhan-gollapalli <20je0358@ee.iitism.ac.in>
2024-01-03 18:37:56 +0530

    03-01-2024

--- redux/backend/db.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require('mongoose');
-const mongoURI= "mongodb://localhost:27017/inotes"
-const connectToMongo= async ()=>{
-    await mongoose.connect(mongoURI);
-    console.log("Connected to Mongo")
-}
-module.exports = connectToMongo;
-
--- redux/src/notes/store.js
+++
@@ -1,8 +0,0 @@
-import { configureStore } from '@reduxjs/toolkit'
-import noteReducer from './noteSlice';
-
-export const store = configureStore({
-    reducer:{
-        notes:noteReducer,
-    }
-})
